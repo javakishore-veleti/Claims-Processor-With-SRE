a3990b8703302a4fe5f36f72089b476d7407867a
javakishore-veleti <javakishore@gmail.com>
2026-03-14 21:35:38 -0400

    Fix WebConfig: Batch/Tenants/Entitlements/SRE use static/ not static/browser/

--- Portal-SRE/src/main/java/com/healthcare/claims/portal/sre/config/WebConfig.java
+++ Portal-SRE/src/main/java/com/healthcare/claims/portal/sre/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/browser/");
+                .addResourceLocations("classpath:/static/");
     }
 
     @Override
